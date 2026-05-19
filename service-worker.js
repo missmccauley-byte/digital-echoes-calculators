17a6aff161eafb115444e6c08fa2e164836453ec
missmccauley-byte <missmccauley@gmail.com>
2026-05-18 20:06:21 -0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,5 @@
+const CACHE_NAME='de-money-risk-v50';
+const ASSETS=['./','./index.html','./manifest.json','./icon.svg'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME?caches.delete(k):null))));self.clients.claim();});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));});
